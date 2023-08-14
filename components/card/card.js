7dee405bee6c78d499177da4916ce07b7c1172e3
uzmahashmi <uzma.hashmi@outlook.com>
2023-08-14 10:26:25 +0200

    add crateCharacterCard function

--- components/card/card.js
+++ components/card/card.js
@@ -1,1 +1,27 @@
-export function createCharacterCard() {}
+export function createCharacterCard(url, status, type, occurrences) {
+  //const card = document.querySelector("[data-js=card-container]");
+
+  const li = document.createElement("li");
+  li.innerHTML = ` 
+    <div class="card__image-container">
+      <img
+        class="card__image"
+        src=${url}
+        alt="Rick Sanchez"
+      />
+      <div class="card__image-gradient"></div>
+    </div>
+    <div class="card__content">
+      <h2 class="card__title">Rick Sanchez</h2>
+      <dl class="card__info">
+        <dt class="card__info-title">Status</dt>
+        <dd class="card__info-description">${status}</dd>
+        <dt class="card__info-title">Type</dt>
+        <dd class="card__info-description">${type}</dd>
+        <dt class="card__info-title">Occurrences</dt>
+        <dd class="card__info-description">${occurrences}</dd>
+      </dl>
+    </div>`;
+
+  return li;
+}
